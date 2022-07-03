7585b3bd870492673f082194653fcc17f5833d5f
kezeni <kenzeni@daum.net>
2022-07-03 23:02:00 +0900

    1

--- zen crawling/request.py
+++ zen crawling/request.py
@@ -1,9 +1,20 @@
 import requests
 
-res = requests.get("https://www.naver.com/")
-print("응답코드 :", res.status_code)
+res = requests.get("https://www.google.com/")
 
-if res.status_code == requests.codes.ok:
-    print("정상입니다")
-else:
-    
+# res = requests.get("https://www.naver.com/")
+res.raise_for_status()
+
+# print("응답코드 :", res.status_code)
+
+# if res.status_code == requests.codes.ok:
+#     print("정상입니다")
+# else:
+#     print("에러코드", res.status_code)
+
+print(len(res.text))
+
+print(res.text)
+
+with open("mygoogle.html", "w", encoding="utf8") as f:
+    f.write(res.text)
